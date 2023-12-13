8385040e7b5f47c8977e5919311f5a36afd3ae2d
MooozzZ <1362610792@qq.com>
2023-12-13 23:23:15 +0800

    feature: add intercepting router

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
-export default function Page () {
-  return <h1>/app/page.js</h1>
+// app/page.js
+import Link from 'next/link'
+import { photos } from './data'
+
+export default function Home () {
+  debugger
+  return (
+    <main className='container'>
+      {photos.map(({ id, src }) => (
+        <Link key={id} href={`/photo/${id}`}>
+          <img width='100' src={src} />
+        </Link>
+      ))}
+    </main>
+  )
 }
